a73ba6af03b735922e11b3d0863a7fbb82727f04
Irrisorr <zakharkevichg@gmail.com>
2024-04-05 09:24:09 +0200

    add rand func

--- 1_ListView_App/app/src/main/java/com/example/a1_listview_app/MainActivity.kt
+++ 1_ListView_App/app/src/main/java/com/example/a1_listview_app/MainActivity.kt
@@ -7,24 +7,34 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.a1_listview_app.databinding.ActivityMainBinding
+import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    private var random_num = 1
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        binding.button.setOnClickListener {
-            Toast.makeText(applicationContext, "Pressed button", Toast.LENGTH_SHORT).show()
+        if (savedInstanceState == null) {
+            binding.textView.text = random_num.toString()
+            val random_num = savedInstanceState?.getInt("RAND")
+            if (random_num != null) {
+                this.random_num = random_num
+            }
         }
 
-        binding.button.setOnLongClickListener{
-            Toast.makeText(applicationContext, "Pressed long", Toast.LENGTH_SHORT).show()
-            true
+        binding.button.setOnClickListener {
+            random_num = Random.nextInt(0, 10)
+            binding.textView.text = random_num.toString()
         }
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putInt("RAND", random_num)
     }
 }
