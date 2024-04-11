b4cf989ccafbfb42165bf297b9cd8450231fa1d2
Irrisorr <zakharkevichg@gmail.com>
2024-04-11 16:49:55 +0200

    2nd lab

--- 1_ListView_App/app/src/main/java/com/example/a1_listview_app/MainActivity.kt
+++ 1_ListView_App/app/src/main/java/com/example/a1_listview_app/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.a1_listview_app
 
 import android.os.Bundle
+import android.widget.ArrayAdapter
 import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
@@ -10,21 +11,20 @@ import androidx.fragment.app.commit
 import com.example.a1_listview_app.databinding.ActivityMainBinding
 import kotlin.random.Random
 
+private lateinit var binding: ActivityMainBinding
 class MainActivity : AppCompatActivity() {
-
-    private lateinit var binding: ActivityMainBinding
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityMainBinding.inflate(layoutInflater)
+        binding =ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        val arrayAdapter: ArrayAdapter<*>
+        val languages = arrayOf(
+            "Python", "Java", "C#","C++"
+        )
+        //deklaruje zmienną niemutowalną o nazwie arrayAdapter o typie
+        var mListView = binding.listview
 
-        val firstFrag = BlankFragment()
-        val secondFrag = BlankFragment2()
-        supportFragmentManager.commit {
-            add(R.id.Fragment1, firstFrag)
-            add(R.id.Fragment2, secondFrag)
-        }
-
+        arrayAdapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, languages)
+        mListView.adapter = arrayAdapter
     }
 }
