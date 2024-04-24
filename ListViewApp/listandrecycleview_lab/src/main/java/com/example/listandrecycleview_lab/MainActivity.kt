fc88f0c049bf9e9e3f559ab9a9537dd87e8f28ca
Irrisorr <zakharkevichg@gmail.com>
2024-04-24 19:07:22 +0200

    example listview with adapter

--- ListViewApp/listandrecycleview_lab/src/main/java/com/example/listandrecycleview_lab/MainActivity.kt
+++ ListViewApp/listandrecycleview_lab/src/main/java/com/example/listandrecycleview_lab/MainActivity.kt
@@ -1,20 +1,27 @@
 package com.example.listandrecycleview_lab
 
 import android.os.Bundle
+import android.widget.ArrayAdapter
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.listandrecycleview_lab.databinding.ActivityMainBinding
 
+private lateinit var binding: ActivityMainBinding
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+        binding =ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        val arrayAdapter: ArrayAdapter<*>
+        val languages = arrayOf(
+            "Python", "Java", "C#","C++"
+        )
+        //deklaruje zmienną niemutowalną o nazwie arrayAdapter o typie
+        var mListView = binding.listview
+
+        arrayAdapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, languages)
+        mListView.adapter = arrayAdapter
     }
 }
