a3c870c336c8ba083afff371d15e574085f9f62f
Heorhi (George) Zakharkevich <102664241+Irrisorr@users.noreply.github.com>
2024-04-24 18:25:40 +0200

    Delete TouristRoute directory

--- TouristRoute/app/src/main/java/com/example/touristroute/User.kt
+++
@@ -1,4 +0,0 @@
-package com.example.touristroute
-
-data class User(var title : String, var description : String, var time : String,
-                var phone : String, var country : String, var imageId : Int)
